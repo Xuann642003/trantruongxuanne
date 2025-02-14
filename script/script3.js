353e9f8ffde9ebd6e630cd8a555008a20ef244f8
Xuan <xfuiaznzz@gmail.com>
2025-02-14 21:24:24 +0700

    Xuan

--- script/script3.js
+++ script/script3.js
@@ -6,3 +6,12 @@ document.getElementById("toggle-icon-heart").addEventListener("click", function 
         content.style.display = "none"; // Ẩn div
     }
 });
+
+document.getElementById("toggle-icon-star").addEventListener("click", function () {
+    var content = document.getElementById("cons-6-1");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // Hiện div
+    } else {
+        content.style.display = "none"; // Ẩn div
+    }
+});
